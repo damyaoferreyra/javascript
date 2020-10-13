e652de34b58e5b57548425e1d5e06bf6318aa446
Damiao Ferreira <damyaoferreyra@hotmail.com>
2020-10-13 15:19:51 -0300

    Implements: destruction not working. solved

--- course nodejs innovation one/nodejs/tarefas_background_nodejs_and_redis/src/app/controllers/UserController.js
+++ course nodejs innovation one/nodejs/tarefas_background_nodejs_and_redis/src/app/controllers/UserController.js
@@ -3,11 +3,10 @@ import Queue from  '../lib/Queue';
 
 export default {
     async store(req, res) {
-        const { name, email } = req.body
 
         const user = {
-            name,
-            email,
+            name: req.body.user.name,
+            email: req.body.user.email,
             password : passwordGenerator(15, false)
         }
 
